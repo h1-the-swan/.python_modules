5ad3330b69ec2bf02c4d10ccf2037cd07d3933ab
h1-the-swan <jason.portenoy@gmail.com>
2016-04-30 19:08:26 +0000

    methods to support interdisciplinarity work

--- DBConnectMAG.py
+++ DBConnectMAG.py
@@ -15,6 +15,10 @@ class DBConnectMAG(MySQLConnect):
         self.tblname_nodes = 'Papers'
         self.colname_venue = ['Journal_ID_mapped_to_venue_name', 'Conference_series_ID_mapped_to_venue_name']
         self.colname_paperid = 'Paper_ID'
+        
+        self.tblname_links = 'PaperReferences'
+        self.colname_citing = 'Paper_ID'
+        self.colname_cited = 'Paper_reference_ID'
 
     def _get_table(self, t):
         """If passed a string, get the table object
@@ -42,26 +46,70 @@ class DBConnectMAG(MySQLConnect):
             return col
 
     def query_for_venue_counts(self, paperids, 
-            table=self.tblname_nodes, 
-            col_venue=self.colname_venue[0], 
-            col_paperid=self.colname_paperid):
-        """TODO: given a paperid or list of paperids, return the counts by venue
-        Venues are e.g. journals, and is specified by the 'col_venue' param
+            table=None, 
+            col_venue=None, 
+            col_paperid=None):
+        """given a paperid or list of paperids, return the counts by venue
+        Venues are e.g. journals, specified by the 'col_venue' param
 
         :paperids: paperid or list of paperids
         :table: table object or name of table
         :col_venue: column (or name of column) to count
         :col_paper: column (or name of column) with paper ids
-        :returns: SQLAchemy response
+        :returns: SQLAchemy response with columns (count, venue_id)
 
         """
+        if table is None:
+            table=self.tblname_nodes
+        if col_venue is None:
+            col_venue = self.colname_venue[0]
+        if col_paperid is None:
+            col_paperid = self.colname_paperid
         paperids = parse_id(paperids)
         tbl = self._get_table(table)
-        col_venue = self._get_col(col_venue)
-        col_paperid = self._get_col(col_paperid)
+        col_venue = self._get_col(col_venue, tbl)
+        col_paperid = self._get_col(col_paperid, tbl)
         sq = tbl.count(col_paperid.in_(paperids))
         sq = sq.where(col_venue>0)  # TODO: THIS MAY NOT WORK WITH STRING IDS
         sq = sq.group_by(col_venue)
         sq = sq.column(col_venue)
-        r = self.db.engine.execute(sq)
+        r = self.engine.execute(sq)
         return r
+
+    def query_for_citations(self, paperids, 
+            direction='out', 
+            table=None, 
+            col_citing=None, 
+            col_cited=None, 
+            return_df=False):
+        """Take a list of paper ids and return the citations as either a Series or DataFrame
+
+        :paperids: paperid or list of paperids
+        :direction: 'out' (default) for out-citations. 'in' for incoming citations
+        :table: table object or name of table
+        :col_citing: column (or name of column) for citing papers
+        :col_cited: column (or name of column) for cited papers
+        :return_df: return a dataframe rather than a series (default False)
+        :returns: either a Series of paper IDs or the DataFrame of both citing/cited
+
+        """
+        if table is None:
+            table = self.tblname_links
+        if col_citing is None:
+            col_citing = self.colname_citing
+        if col_cited is None:
+            col_cited = self.colname_cited
+        paperids = parse_id(paperids)
+        tbl = self._get_table(table)
+        col_citing = self._get_col(col_citing, tbl)
+        col_cited = self._get_col(col_cited, tbl)
+        cols = (col_citing, col_cited)
+        if direction.lower() == 'in':
+            cols = (col_cited, col_citing)
+        sq = tbl.select(cols[0].in_(paperids))
+        sq = sq.with_only_columns(cols)
+        result = self.read_sql(sq)
+        if return_df is True:
+            return result
+        else:
+            return result.ix[:, 1]
